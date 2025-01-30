38e7b66c3a4937202744f728e9d3361d89b63e28
JuanMa Garcia <juanmgar@gmail.com>
2025-01-30 20:47:05 +0100

    [desin 1.1] Permitir crear usuarios con todos los campos que hay en la base de datos
    Se añaden el resto de campos al formulario para poder persistirlos

--- src/Components/User/CreateUserComponent.js
+++ src/Components/User/CreateUserComponent.js
@@ -1,4 +1,4 @@
-import {useState, useRef} from "react";
+import {useState} from "react";
 import {modifyStateProperty} from "../../Utils/UtilsState";
 
 let CreateUserComponent = () => {
@@ -6,12 +6,20 @@ let CreateUserComponent = () => {
     let [formData, setFormData] = useState({
         email: '',
         password: '',
-    })
+        name: '',
+        surname: '',
+        documentIdentity: '',
+        documentNumber: '',
+        country: '',
+        address: '',
+        postalCode: '',
+        birthday: ''
+    });
 
     let clickCreate = async () => {
         let response = await fetch(process.env.REACT_APP_BACKEND_BASE_URL+"/users", {
             method: "POST",
-            headers: {"Content-Type": "application/json "},
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify(formData)
         })
 
@@ -32,12 +40,34 @@ let CreateUserComponent = () => {
             <h2>Create User</h2>
             <input onChange={(i) => {
                 modifyStateProperty(formData, setFormData, "email", i.currentTarget.value)
-            }}
-                   type="text" name="email"/>
+            }} type="text" name="email" placeholder="Email" required/>
             <input onChange={(i) => {
                 modifyStateProperty(formData, setFormData, "password", i.currentTarget.value)
-            }}
-                   type="password" name="password"/>
+            }} type="password" name="password" placeholder="Contraseña" required/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "name", i.currentTarget.value)
+            }} type="text" name="name" placeholder="Nombre"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "surname", i.currentTarget.value)
+            }} type="text" name="surname" placeholder="Apellido"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "documentIdentity", i.currentTarget.value)
+            }} type="text" name="documentIdentity" placeholder="Tipo de documento"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "documentNumber", i.currentTarget.value)
+            }} type="text" name="documentNumber" placeholder="Número de documento"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "country", i.currentTarget.value)
+            }} type="text" name="country" placeholder="País"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "address", i.currentTarget.value)
+            }} type="text" name="address" placeholder="Dirección"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "postalCode", i.currentTarget.value)
+            }} type="text" name="postalCode" placeholder="Código postal"/>
+            <input onChange={(i) => {
+                modifyStateProperty(formData, setFormData, "birthday", i.currentTarget.value)
+            }} type="text" name="birthday" placeholder="Fecha de nacimiento"/>
             <button onClick={clickCreate}>Create User</button>
         </div>
     )
